8f9124bca89a9f6401a41ec104db51426dc1e36a
Subhash Kanaparthi <57779223+sckanaparthi@users.noreply.github.com>
2023-03-23 14:47:24 -0400

    fixed card

--- KhunPoker/src/util/Card.cpp
+++ KhunPoker/src/util/Card.cpp
@@ -4,10 +4,10 @@
  * @file Card.cpp
  * @brief Holds the card representation consisting of rank and a suit.
  */
-Card::Card(Rank r, Suit s) : card{Card::RANKS_AS_CHAR[r], Card::SUITS_AS_CHAR[s], '\0'}, rank(r), suit(s) {};
+Card::Card(Rank r, Suit s) : cardAsString{Card::RANKS_AS_CHAR[r], Card::SUITS_AS_CHAR[s], '\0'}, rank(r), suit(s) {};
 
 std::string Card::toString() const {
-    return this->card;
+    return this->cardAsString;
 }
 
 Rank Card::getRank() const {
@@ -21,6 +21,6 @@ Suit Card::getSuit() const {
 void Card::operator=(const Card& card) {
     this->rank = card.getRank();
     this->suit = card.getSuit();
-    this->card[0] = Card::RANKS_AS_CHAR[card.getRank()];
-    this->card[1] = Card::SUITS_AS_CHAR[card.getSuit()];
+    this->cardAsString[0] = Card::RANKS_AS_CHAR[card.getRank()];
+    this->cardAsString[1] = Card::SUITS_AS_CHAR[card.getSuit()];
 }
