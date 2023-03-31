d00cfe93da478a24be555a5b642d11bea4416847
r4c3 <rqwilliams9@gmail.com>
2023-03-31 18:11:30 -0400

    format GameState.h

--- KhunPoker/include/game/GameState.h
+++ KhunPoker/include/game/GameState.h
@@ -1,43 +1,34 @@
-/**
- * @file GameState.h
- * @brief contains the GameState struct, see brief below.
- */
-
-#ifndef KHUNSOLVER_INCLUDE_GAMESTATE_H_
-#define KHUNSOLVER_INCLUDE_GAMESTATE_H_
+#ifndef GAMESTATE_H_
+#define GAMESTATE_H_
 
 #include <util/Deck.h>
+#include <util/Player.h>
 #include <util/Street.h>
 
-
-/** 
+/**
  * @struct GameState
  * @brief holds all game state logic, similar to Rule in TexasSolver
- * 
+ *
  * See TexasSolver's implemenetation at:
  * https://github.com/bupticybee/TexasSolver/blob/master/include/tools/Rule.h
- */
-struct GameState
-{
-  Deck deck;
-
-  Street street; //the current street (preflop, flop, turn, river)
-
-  float oop_commit; //the out of position player's money in pot
-  float ip_commit; //the in position player's money in pot
-  float effective_stack; //the smallest stack size between two players
+*/
+struct GameState {
+    Deck deck;
+    Street street; //the current street (preflop, flop, turn, river)
+    Player player_turn; //IP or OP
+    float initial_effective_stack; //the smallest stack size between two players
+    float ip_commit; //the in position player's money in pot
+    float oop_commit; //the out of position player's money in pot
 
   GameState(
-    Deck deck,
-    Street street,
+    const Deck& deck,
+    Street& street,
     float oop_commit,
     float ip_commit,
     float initial_effective_stack,
     float allin_threshold,
     Player player_turn
-    // float small_blind, (holdem only)
-    // float big_blind
   );
 };
 
-#endif //KHUNSOLVER_INCLUDE_GAMESTATE_H_
+#endif //GAMESTATE_H_
