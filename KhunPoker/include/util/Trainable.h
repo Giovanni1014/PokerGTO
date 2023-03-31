9c4e6dc0ea4eb3b6b2f24514dbc47cc9e52f2b35
creemr <pranavm04@icloud.com>
2023-03-31 18:03:02 -0400

    Update Trainable.h

--- KhunPoker/include/util/Trainable.h
+++ KhunPoker/include/util/Trainable.h
@@ -1,6 +1,18 @@
 #ifndef TRAINABLE_H
 #define TRAINABLE_H
 
+#include <vector>
+using namespace std;
 
+class Trainable {
+    private:
+        vector<double> regrets;
+
+
+    public:
+        Trainable();
+
+        void updateRegrets(const vector<double> &regrets, int iteration_number, const vector<double> &reach_probabilities);
+};
 
 #endif
