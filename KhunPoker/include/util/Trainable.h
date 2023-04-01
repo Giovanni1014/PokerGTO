24a5793b9ec6c70323e00d0198f48f05820e8470
creemr <pranavm04@icloud.com>
2023-04-01 02:56:12 -0400

    Update Trainable.h

--- KhunPoker/include/util/Trainable.h
+++ KhunPoker/include/util/Trainable.h
@@ -6,12 +6,20 @@ using namespace std;
 
 class Trainable {
     private:
-        vector<double> regrets;
-
+        int action_number;
+        int card_number;
+        vector<float> regrets_positive;
+        vector<float> regrets_positive_sum;
+        vector<float> cumulative_regrets_positive;
+        vector<float> cumulative_regrets_positive_sum;
+        vector<float> regrets;
+        vector<float> return_value;
 
     public:
         Trainable();
-
+        
+        const vector<float> getAverageStrategy();
+        const vector<float> getCurrentStrategy();
         void updateRegrets(const vector<double> &regrets, int iteration_number, const vector<double> &reach_probabilities);
 };
 
