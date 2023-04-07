df7a7ffa0e4e6c6e992db96add0ee0df6d4670b1
Subhash Kanaparthi <57779223+sckanaparthi@users.noreply.github.com>
2023-04-07 16:32:46 -0400

    math change

--- KhunPoker/src/nodes/ShowdownNode.cpp
+++ KhunPoker/src/nodes/ShowdownNode.cpp
@@ -7,7 +7,7 @@ vector<float> ShowdownNode::utility(const Player player, const vector<float>& re
     vector<float> finalUtil(n, 0.0f);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            finalUtil[i] = Math::signum(i - j) * pot;
+            finalUtil[i] = Math::signum(i - j) * reach_probs[i] * (opp_reach_probs[j] / (1 - opp_reach_probs[i])) * pot;
         }
     }
     return finalUtil;
